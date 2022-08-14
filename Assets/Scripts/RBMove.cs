c79fa73dd9e8a7c0e53b3e9413a02836f320822e
SamoUhliar <samo.uhliar1@gmail.com>
2022-08-14 22:07:05 +0200

    rigidbody v1

--- Assets/Scripts/RBMove.cs
+++ Assets/Scripts/RBMove.cs
@@ -6,26 +6,66 @@ public class RBMove : MonoBehaviour
 {
     public float speed = 10f; //Controls velocity multiplier
     public float jump = 10f; //Controls velocity multiplier
+    public float rotateSpeed = 10f; //Controls velocity multiplier
     Rigidbody rb; //Tells script there is a rigidbody, we can use variable rb to reference it in further script
-
+    Vector3 movement;
+    Transform camera;
+    float smoothTurnVelocity;
+    [SerializeField] float smoothTurnTime = 0.2f;
+    [SerializeField] LayerMask groundLayer;
+    int jumps;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); //rb equals the rigidbody on the player
+        camera = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement = new Vector3(Input.GetAxis("Vertical"),0, Input.GetAxis("Horizontal") * -1);
+        Jump();
+        Ground();
+    }
 
-        //Apply the movement vector to the current position, which is
-        //multiplied by deltaTime and speed for a smooth MovePosition
-        rb.MovePosition(transform.position + m_Input * Time.deltaTime * speed);
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    void moveCharacter(Vector3 direction)
+    {
+        // Convert direction into Rigidbody space.
+        direction = rb.rotation * direction;
+        Rotation();
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+    void Rotation()
+    {
+        Quaternion rotation = Quaternion.Lerp(transform.transform.rotation *= Quaternion.Euler(0, -20, 0), camera.transform.rotation, rotateSpeed * Time.deltaTime);
+        rotation.x = 0;
+        rotation.z = 0;
+        //rotation *= Quaternion.Euler(0, 50, 0);
+        transform.rotation = rotation;
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumps > 0)
         {
-            rb.AddForce(Vector3.up * jump);
+            jumps--;
+            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+        }
+    }
+
+    void Ground()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.3f, groundLayer))
+        {
+            jumps = 1;
         }
     }
 }
